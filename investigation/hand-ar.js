d0c63597dd11b86b337d8d8005c8474f29770460
Reg <reg@alink-group.co.jp>
2021-07-16 16:21:25 +0900

    Added model params

--- investigation/hand-ar.js
+++ investigation/hand-ar.js
@@ -1,5 +1,13 @@
 var cameraView;
 
+
+const modelParams = {
+    flipHorizontal: true,   // flip e.g for video  
+    maxNumBoxes: 20,        // maximum number of boxes to detect
+    iouThreshold: 0.5,      // ioU threshold for non-max suppression
+    scoreThreshold: 0.6,    // confidence threshold for predictions.
+}
+
 var constraints = {
     audio: false,
     video: {
