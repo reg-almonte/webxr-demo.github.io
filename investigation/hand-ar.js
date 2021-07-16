8aa033dbd44c0fba5bcfac6a4c538ce608ee8a30
Reg <reg@alink-group.co.jp>
2021-07-16 17:24:48 +0900

    Red cursor: Fixed cameraView

--- investigation/hand-ar.js
+++ investigation/hand-ar.js
@@ -1,4 +1,4 @@
-const cameraView = document.getElementById("webcam");
+cameraView = document.getElementById("webcam");
 // const canvas = document.getElementById("canvas");
 // const context = canvas.getContext("2d");
 
