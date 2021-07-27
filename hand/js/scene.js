b9578d5fde8fcedfb1d5341f85d35d4928127da7
Reg <reg@alink-group.co.jp>
2021-07-28 08:58:42 +0900

    Red text: Intersection code

--- hand/js/scene.js
+++ hand/js/scene.js
@@ -140,13 +140,10 @@ function moveTheRing(value) {
 
 	for ( let i = 0; i < intersects.length; i ++ ) {
 		intersects[ i ].object.material.color.set( 0xff0000 );
-    scene.children[ 0 ].material.color.set( 0xff00ff );
 	}
-  // if(intersects.length == 0) {
-  //   for ( let i = 0; i < scene.children.length; i ++ ) {
-  //     scene.children[ i ].material.color.set( 0xff00ff );
-  //   }
-  // }
+  if(intersects.length == 0) {
+    scene.children[ i ].material.color.set( 0x9999FF );
+  }
   // var intersects = raycaster.intersectObjects( scene.children );
   // if(intersects.length != 0)
   // {
