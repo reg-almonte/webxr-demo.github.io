c5999ca00adba7e63bc8586d1911765da28f323f
Reg <reg@alink-group.co.jp>
2021-07-28 09:24:41 +0900

    Red text: experiment on raycast

--- hand/js/scene.js
+++ hand/js/scene.js
@@ -132,20 +132,24 @@ function moveTheRing(value) {
   ring.position.y = -((window.innerHeight * value.y) / window.innerHeight) * 5;
   renderer.render(scene, camera);
 
-  mouse.x = ( value.x / window.innerWidth ) * 2 - 1;
-	mouse.y = -( value.y / window.innerHeight ) * 2 + 1;
+  mouse.x = ((window.innerWidth * value.x) / window.innerWidth) * 5;
+	mouse.y = -((window.innerHeight * value.y) / window.innerHeight) * 5;
   var raycaster = new THREE.Raycaster();
   raycaster.setFromCamera( mouse, camera );               
-  const intersects = raycaster.intersectObjects( cube );
 
-	// for ( let i = 0; i < intersects.length; i ++ ) {
-	// 	intersects[ i ].object.material.color.set( 0xff0000 );
-	// }
+  const intersects = raycaster.intersectObjects( scene.children );
+	for ( let i = 0; i < intersects.length; i ++ ) {
+		intersects[ i ].object.material.color.set( 0xff0000 );
+	}
+
+  //const intersects = raycaster.intersectObjects( cube );
   if(intersects.length == 0) {
     cube.material.color.set( 0x9999FF );
-  } else {
-    cube.object.material.color.set( 0xff0000 );
   }
+  // else {
+  //   cube.object.material.color.set( 0xff0000 );
+  // }
+  
   // var intersects = raycaster.intersectObjects( scene.children );
   // if(intersects.length != 0)
   // {
